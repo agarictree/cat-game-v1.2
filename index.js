b95dc77911045473e7d55b0f968178508b648b85
Екатерина <ormhild@gmail.com>
2021-07-20 22:12:05 +0300

    Добавление счетчика
    Добавление счетчика;
    Добавление блока "конец игры";

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ let container = document.getElementById("background");
 let width = container.getBoundingClientRect().width;
 let height = container.getBoundingClientRect().height;
 let catCoords = cat.getBoundingClientRect();
+let startBlock = document.getElementById("start");
 let startButton = document.getElementById("startBtn");
+let fishCountBlock = document.getElementById("fishCount");
+let timeBlock = document.getElementById("time");
+let endBlock = document.getElementsByClassName("end")[0];
+let fishCount = 0;
+let time = 60;
 
 function addKeypress(event) {
     let ar = "ArrowRight";
@@ -43,6 +49,12 @@ function addFish(path, id) {
     return fish;
 }
 
+function returnPoint(x, y) {
+    let points = document.elementsFromPoint(x, y);
+    let point = points.find((elem) => elem.id == "cat");
+    return point;
+}
+
 let arr = [["images/02.png", "bad"], ["images/03.png", "fresh"]];
 
 let randomFish = getRandomIntInclusive(0, arr.length - 1);
@@ -50,26 +62,62 @@ let [path, id] = arr[randomFish];
 console.log(path, id);
 
 function fishfall(path, id) {
+
     let fish = addFish(path, id);
+
     setInterval(() => {
+
+        if(fishCount >= 0) {
         let fTop = parseFloat(getComputedStyle(fish).top);
         let fHeight = fish.offsetHeight;
-        // let fLeft = parseFloat(getComputedStyle(fish).left);
-        let fCoords = fish.getBoundingClientRect();
-       let points = document.elementsFromPoint(fCoords.x, fCoords.y);
-       let point = points.find((elem) => elem.id == "cat");
-    //    console.log(point);
+    
         fish.style.top = fTop + 10 + "px";
-        if(fTop >= (height - fHeight) || point) {
+    
+        if(fTop >= (height - fHeight)) {
             let newRandomFish = getRandomIntInclusive(0, arr.length - 1);
             let [newPath, newId] = arr[newRandomFish];
             fish.remove();
            fish = addFish(newPath, newId);
-        };
+        }
+    }
+
+        let fCoords = fish.getBoundingClientRect();
+        let finalPoint = returnPoint(fCoords.x, fCoords.y);
+
+        if(finalPoint) {
+            let fId = fish.id;
+
+            if(fId == "fresh") {
+                fishCount += 1;
+                fishCountBlock.textContent = fishCount;
+            };
+
+            if(fId == "bad") {
+                fishCount -= 1;
+                fishCountBlock.textContent = fishCount;
+            };
+            
+            if(fishCount < 0) {
+                fishCountBlock.textContent = 0;
+                document.body.append(startBlock);
+                endBlock.classList.remove("none");
+                fish.remove();
+                fishfall = false;
+            }
+
+            let newRandomFish = getRandomIntInclusive(0, arr.length - 1);
+            let [newPath, newId] = arr[newRandomFish];
+            fish.remove();
+           fish = addFish(newPath, newId);
+        }
+    console.log(fishCount);
     }, 100);
 }
+
+
 function startGame(e) {
     e.target.parentElement.remove();
+    fishCount = 0;
     fishfall(path, id);
 }
 startButton.addEventListener("click", startGame);
